48c9aae8da040289d950cf30aa4581856bd9c9fc
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-01-02 19:58:43 +0200

    testing 2

--- 01.SchoolSupplies/Program.cs
+++ 01.SchoolSupplies/Program.cs
@@ -6,7 +6,12 @@ namespace SchoolSupplies
     {
         public static void Main()
         {
-            
+            int numberPens = int.Parse(Console.ReadLine());
+            int numberMarkers = int.Parse(Console.ReadLine());
+            double litresCleaner = double.Parse(Console.ReadLine());
+            double discount = double.Parse(Console.ReadLine()) * 0.01;
+
+
         }
     }
 }
