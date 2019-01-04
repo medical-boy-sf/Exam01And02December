87e14040a76bda5d7f63a4fa0e7f02ff7e5a524b
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-01-04 16:45:30 +0200

    04. Everest

--- 04.Everest/Program.cs
+++ 04.Everest/Program.cs
@@ -6,6 +6,35 @@ namespace _04.Everest
     {
         static void Main()
         {
+            int initialHeight = 5364;
+            int days = 1;
+            string yesOrNo = "";
+            string height = "";
+            int heightInt = 0;
+            int goal = 8848;
+
+            while (true)
+            {
+                yesOrNo = Console.ReadLine().ToLower();
+                if (yesOrNo == "yes")
+                {
+                    days += 1;
+                }
+                if (days > 5 || initialHeight >= goal || yesOrNo == "end") break;
+                height = Console.ReadLine().ToLower();
+                heightInt = int.Parse(height);
+                initialHeight += heightInt;
+            }
+
+            if (initialHeight >= goal)
+            {
+                Console.WriteLine($"Goal reached for {days} days!");
+            }
+
+            else
+            {
+                Console.WriteLine($"Failed!\n{initialHeight}");
+            }
         }
     }
 }
