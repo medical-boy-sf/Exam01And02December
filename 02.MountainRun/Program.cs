e3f2fe8217ef09aa1f274054ed5d77137a6517af
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-01-04 10:24:57 +0200

    02.Mountain run

---
+++ 02.MountainRun/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _02.MountainRun
+{
+    class Program
+    {
+        static void Main()
+        {
+            double record = double.Parse(Console.ReadLine());
+            double distance = double.Parse(Console.ReadLine());
+            double timeForOneMeter = double.Parse(Console.ReadLine());
+
+            double time = distance * timeForOneMeter;
+            double timeSlowing = Math.Floor((distance / 50)) * 30;
+            double totalTime = time + timeSlowing;
+
+            if (totalTime < record)
+            {
+                Console.WriteLine($"Yes! The new record is {totalTime:F2} seconds.");
+            }
+
+            else
+            {
+                Console.WriteLine($"No! He was {(totalTime - record):F2} seconds slower.");
+            }
+        }
+    }
+}
