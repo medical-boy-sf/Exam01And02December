2270e3659c1942a3a3d3f109c6bff45568972e7e
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-01-04 19:14:16 +0200

    05. Project prize

---
+++ 05.ProjectPrize/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _05.ProjectPrize
+{
+    class Program
+    {
+        static void Main()
+        {
+            int projectParts = int.Parse(Console.ReadLine());
+            double prize = double.Parse(Console.ReadLine());
+            int points = 0;
+            int sum = 0;
+
+            for (int i = 1; i <= projectParts; i++)
+            {
+                points = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    sum += points * 2;
+                }
+
+                else
+                {
+                    sum += points;
+                }
+            }
+
+            prize *= sum;
+            Console.WriteLine($"The project prize was {prize:F2} lv.");
+        }
+    }
+}
