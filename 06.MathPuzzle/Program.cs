f1622f998a26d30ad71a35a8e637fea54720d0f8
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-01-04 21:34:57 +0200

    06. Math puzzle

--- 06.MathPuzzle/Program.cs
+++ 06.MathPuzzle/Program.cs
@@ -6,7 +6,34 @@ namespace _06.MathPuzzle
     {
         static void Main()
         {
+            int number = int.Parse(Console.ReadLine());
+            int counter = 0;
 
+            for (int a = 1; a <= 30; a++)
+            {
+                for (int b = 1; b <= 30; b++)
+                {
+                    for (int c = 1; c <= 30; c++)
+                    {
+                        if (a * b * c == number && (a > b && b > c))
+                        {
+                            Console.WriteLine($"{a} * {b} * {c} = {number}");
+                            counter += 1;
+                        }
+
+                        if (a + b + c == number && (c > b && b > a))
+                        {
+                            Console.WriteLine($"{a} + {b} + {c} = {number}");
+                            counter += 1;
+                        }
+                    }
+                }
+            }
+
+            if (counter == 0)
+            {
+                Console.WriteLine("No!");
+            }
         }
     }
 }
