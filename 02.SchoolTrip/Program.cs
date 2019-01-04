d4e4f2bca9ec70ca7764f3b18b719842c6d83392
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-01-04 10:57:42 +0200

    02.School trip

---
+++ 02.SchoolTrip/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _02.SchoolTrip
+{
+    class Program
+    {
+        static void Main()
+        {
+            int daysAniMissing = int.Parse(Console.ReadLine());
+            double foodInKg = int.Parse(Console.ReadLine());
+            double firstDog = double.Parse(Console.ReadLine());
+            double secondDog = double.Parse(Console.ReadLine());
+            double thirdDog = double.Parse(Console.ReadLine());
+
+            double totalKg = daysAniMissing * firstDog + daysAniMissing * secondDog + daysAniMissing * thirdDog;
+
+            if (foodInKg >= totalKg)
+            {
+                Console.WriteLine($"{Math.Floor(foodInKg - totalKg)} kilos of food left.");
+            }
+
+            else
+            {
+                Console.WriteLine($"{Math.Ceiling(totalKg - foodInKg)} more kilos of food are needed.");
+            }
+        }
+    }
+}
