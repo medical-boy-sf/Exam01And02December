5e1628d566a6b752ede572c030ebf28a80dc4a7a
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-01-04 08:52:41 +0200

    01.Trekking equipment

--- 01.TrekkingEquipment/Program.cs
+++ 01.TrekkingEquipment/Program.cs
@@ -6,7 +6,15 @@ namespace TrekkingEquipment
     {
         public static void Main()
         {
+            int numberOfAlpinists = int.Parse(Console.ReadLine());
+            int numberCarabiners = int.Parse(Console.ReadLine());
+            int numberRopes = int.Parse(Console.ReadLine());
+            int numbersPickels = int.Parse(Console.ReadLine());
 
+            double sum = numberCarabiners * 36 + numberRopes * 3.6 + numbersPickels * 19.8;
+            double totalSum = sum * numberOfAlpinists;
+            totalSum = totalSum + totalSum * 0.2;
+            Console.WriteLine($"{totalSum:F2}");
         }
     }
 }
