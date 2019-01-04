329e6f03e2854665e41068f56c46382d9b1446b6
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-01-04 18:00:25 +0200

    04. Puppy care

---
+++ 04.PuppyCare/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _04.PuppyCare
+{
+    class Program
+    {
+        static void Main()
+        {
+            int foodInKg = int.Parse(Console.ReadLine()) * 1000;
+
+            string foodADay = Console.ReadLine().ToLower();
+            int sumFood = 0;
+
+            while (foodADay != "adopted")
+            {
+                int foodADayInt = int.Parse(foodADay);
+                sumFood += foodADayInt;
+                foodADay = Console.ReadLine().ToLower();
+            }
+
+            if (foodInKg >= sumFood)
+            {
+                Console.WriteLine($"Food is enough! Leftovers: {foodInKg - sumFood} grams.");
+            }
+
+            else
+            {
+                Console.WriteLine($"Food is not enough. You need {sumFood - foodInKg} grams more.");
+            }
+        }
+    }
+}
