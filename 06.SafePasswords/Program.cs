6503b56e3322baea2534db2761fea51a92f7fdb4
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-01-06 22:51:35 +0200

    06. Safe passwords

---
+++ 06.SafePasswords/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _06.SafePasswords
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int maxNumberPasswords = int.Parse(Console.ReadLine());
+            int counter = 0;
+            char i = '#';
+            char j = '@';
+
+            for (int k = 1; k <= a; k++)
+            {
+                for (int d = 1; d <= b; d++)
+                {
+                    if (i > '7')
+                    {
+                        i = '#';
+                    }
+
+                    if (j > '`')
+                    {
+                        j = '@';
+                    }
+
+                    if (counter < maxNumberPasswords)
+                    {
+                        Console.Write($"{i}{j}{k}{d}{j}{i}|");
+                    }
+
+                    else
+                    {
+                        return;
+                    }
+                    j++;
+                    i++;
+                    counter++;
+                }
+            }
+        }
+    }
+}
